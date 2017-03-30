2ca4f06585e684132e187574d9950c76c37824f6
15810275844 <759095332@qq.com>
2017-03-30 17:36:33 +0800

    修改页面

--- src/admin/Chang/Chang.vue
+++ src/admin/Chang/Chang.vue
@@ -1,14 +1,33 @@
 <template>
     <div>
-       <h4 class="text-center center-block">{{detail.name}} </h4>
-       <ul class="list-group">
-            <li class="list-group-item">
-                <video controls="controls " class="img-responsive" :src="detail.url"></video>
-            </li>
-            <li class="list-group-item">名称：{{detail.name}}</li>
-            <li class="list-group-item">描述：{{detail.description}}</li>
-            <li class="list-group-item">时间：{{detail.createTimer}}</li>
-        </ul>
+        <h4 class="text-center center-block">后台修改页</h4>
+        <div class="back" @click="$router.go(-1)">返回</div>
+        <div class="padding">
+             <form class="bs-example bs-example-form" role="form">
+                <div class="input-group input-group-sm">
+                    <span class="input-group-addon">名称</span>
+                    <input type="text" class="form-control" placeholder="" v-model="detail.name">
+                </div>
+                <br>
+                <div class="input-group input-group-sm">
+                    <span class="input-group-addon">描述</span>
+                    <input type="text" class="form-control" placeholder="" v-model="detail.description">
+                </div>
+                <br>
+                <div class="input-group input-group-sm">
+                    <span class="input-group-addon">地址</span>
+                    <input type="text" class="form-control" placeholder="" v-model="detail.url">
+                </div>
+                <br>
+                 <div class="input-group input-group-sm">
+                    <span class="input-group-addon">头图地址</span>
+                    <input type="text" class="form-control" placeholder="" v-model="detail.cover">
+                </div>
+                <div class="text-center bt-btn">
+                    <button class="btn btn-default col-xs-12" type="button" @click="to">修改录入</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -19,10 +38,39 @@ export default {
             detail:""
         }
     },
-    created (){
-        this.detail = this.$route.params;
-    },
-    methods: {
+    methods:{
+        to () {
+            var test = {
+                name:this.name,
+                description:this.description,
+                url:this.url,
+                cover:this.headUrl
+            };
+            if(!test.name){
+                alert('请输入名称');
+                return;
+            }
+            if(!test.cover){
+                alert('请输入头图地址');
+                return;
+            }
+            if (!test.description){
+                alert('请输入描述');
+                return;
+            }
+            if (!test.url){
+                alert('请输入地址');
+                return;
+            };
+            this.$http.post('/api/add',test).then((data)=>{
+                console.log(data);
+                if(data.data==1){
+                    alert('录入成功');
+                };
+            }).catch((data)=>{
+                console.log(data);
+            });
+        },
         init () {
             this.$http.get('/api/list/'+this.$route.params.id).then(data=>{
                 console.log(data);
@@ -32,12 +80,27 @@ export default {
             });
         }
     },
-    mounted (){
+    mounted(){
         this.init();
     }
 }
 </script>
 
 <style scoped>
+    .padding{
+        padding:10px;
+        padding-top:0;
+    }
+    .bt-btn{
+        padding-top:10px;
+    }
+    .back{
+        height:40px;
+        line-height:40px;
+        background:#fff;
+        padding-left:20px;
+        margin-bottom:20px;
+    }
+
 </style>
 
